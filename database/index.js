450b007b63b6372db14028068d91434802605196
jbweee <bui.just@gmail.com>
2018-09-22 16:25:31 -0700

    username to postgres

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('starkloud', 'jbui', 'stark', {
+const connection = new Sequelize('starkloud', 'postgres', '', {
   host: 'db',
   dialect: 'postgres'
 });
